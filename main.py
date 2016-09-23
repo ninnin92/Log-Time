d9695d7a83681ee60d338256d45e9ae1611ed904
ninnin92 <ninnin92.28@gmail.com>
2016-09-23 23:37:43 +0900

    kviyアプリ - イベントの時刻を記録： ver 1.1 beta ： 新機能に合わせてUI刷新

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import datetime as dt
 
 # PC用の設定
 Config.set('graphics', 'width', '800')  # 画面:横の大きさ
-Config.set('graphics', 'height', '800')  # 画面:縦の大きさ
+Config.set('graphics', 'height', '1000')  # 画面:縦の大きさ
 Config.set('graphics', 'show_cursor', '1')  # マウスカーソルの有無
 
 
@@ -34,7 +34,9 @@ class MyApp(App):
     data = []       # データ置き場
     file_check = 0  # 現在開いているファイルがあるかどうかをスイッチする変数
 
-    def open_log(self):
+    def run_log(self):
+        # ボタンの色を変更（青）
+        self.root.sys1.color = [0.1098, 0.5137, 0.7333, 1]
         # ボタンを押した時間
         self.app_time = "{0:%y-%m-%d_%H-%M-%S}".format(dt.datetime.now())  # formatを使うと一行で済んで便利
         self.fp = open("Log/" + "Log_" + self.app_time + ".csv", "w")  # csvファイルを起動時刻で作成
@@ -45,14 +47,17 @@ class MyApp(App):
             self.fp.write("%s," % i)
         self.fp.write("\n")
         self.file_check = 1  # ファイルが開いた状態
-        print("Open!")
+        print("Run!")
 
         # メッセージの書き出し
         self.root.timel3.text = self.root.timel2.text
         self.root.timel2.text = self.root.timel1.text
-        self.root.timel1.text = "Start Log : " + self.app_time + ".csv"
+        self.root.timel1.text = "Run Log : " + self.app_time + ".csv"
 
     def save_log(self):
+        # ボタンの色を変更
+        self.root.sys2.color = [0.1098, 0.5137, 0.7333, 1]
+
         if self.file_check:
             # リストにあるデータを書き込み
             # print (self.data)
@@ -79,30 +84,35 @@ class MyApp(App):
     # ボタンを離した時の処理：文字色を元に戻す
     def bt_release(self, bt):
         if bt == 1:
-            self.root.hit1.color = [0.2, 0.70588, 0.56470, 1]
+            self.root.hit1.color = [0.1098, 0.5137, 0.7333, 1]  # 青
         elif bt == 2:
-            self.root.hit2.color = [0.2, 0.70588, 0.56470, 1]
+            self.root.hit2.color = [0.1098, 0.5137, 0.7333, 1]  # 青
+        elif bt == 3:
+            self.root.hit3.color = [0.1098, 0.5137, 0.7333, 1]  # 青
+        elif bt == 4:
+            self.root.sys1.color = [237/255, 173/255, 11/255, 1]  # オレンジ（実行中）
         else:
-            self.root.hit3.color = [0.2, 0.70588, 0.56470, 1]
+            self.root.sys1.color = [1, 1, 1, 1]  # 白
+            self.root.sys2.color = [1, 1, 1, 1]  # 白
 
-    # ボタンを押した時の処理：ボタンの文字色と背景色を変更、押した時点での時刻を獲得
+    # ボタンを押した時の処理：ボタンの文字色と背景色を変更 & 押した時点での時刻を獲得
     def time_get(self, bt):
         if bt == 1:
-            self.root.hit1.color = [0.8, 0.8, 0.8, 1]
-            bt = self.bt1
+            self.root.hit1.color = [1, 1, 1, 1]  # 白
+            press_bt = self.bt1  # 押されたボタンのイベント名
         elif bt == 2:
-            self.root.hit2.color = [0.8, 0.8, 0.8, 1]
-            bt = self.bt2
+            self.root.hit2.color = [1, 1, 1, 1]  # 白
+            press_bt = self.bt2  # 押されたボタンのイベント名
         else:
-            self.root.hit3.color = [0.8, 0.8, 0.8, 1]
-            bt = self.bt3
+            self.root.hit3.color = [1, 1, 1, 1]  # 白
+            press_bt = self.bt3  # 押されたボタンのイベント名
 
         now = "{0:%y/%m/%d %H:%M:%S}".format(dt.datetime.now())  # ボタンを押した時の時刻を獲得
-        self.data.append([bt] + [now])  # ラベル名と合わせてリストにぶちこむ
+        self.data.append([press_bt] + [now])  # ラベル名と合わせてリストにぶちこむ
         # ログを残しておく作業 : 1つ上のログを１つ下に下げる（単なる書き換え）
         self.root.timel3.text = self.root.timel2.text
         self.root.timel2.text = self.root.timel1.text
-        self.root.timel1.text = "*  " + bt + " | " + now
+        self.root.timel1.text = press_bt + " | " + now
 
         print(self.root.timel1.text)  # PCでのデバック用
 
